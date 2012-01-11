8abe29a705fb5657064a691fbdce0870ec7d6b75
aditya <dev@chimeracoder.net>
2012-01-11 13:56:25 -0500

    Reordered functions and added get/add location functions

--- babar.rb
+++ babar.rb
@@ -109,20 +109,9 @@ module Babar
       array_of_objects_to_post.collect{|task| task.to_json}.to_s
     end
     
-    def query(endpoint, param_map, *args)
+    def get(endpoint, param_map, *args)
       url = "http://api.toodledo.com/2/#{endpoint}/get.php?key=#{self.key};" + parse_params(param_map, *args) 
       JSON.parse(Typhoeus::Request.get(url).body)
-    end
-
-    def query_locations(param_map, *args)
-      #FIXME this is actually an array of locations
-      query("locations", param_map, *args)
-    end
-    
-    def query_tasks(param_map, *args)
-      #Strip off the first element, since it is not a Task
-      array_of_json_tasks = query("tasks", param_map, *args)
-      array_of_json_tasks[1..-1].collect{|json_task| Babar::Task.new(self, json_task)}
     end
 
     def modify(endpoint, action, desired_class,  array_of_hashes, array_field_name, delete_first_result, *args)
@@ -135,10 +124,29 @@ module Babar
       array_of_results.collect{|json_result| desired_class.new(self, json_result}
     end
 
+
     def add(endpoint, desired_class, array_of_hashes, array_field_name, delete_first_result, *args)
       #url = "http://api.toodledo.com/2/#{endpoint}/add.php?key=#{self.key};#{array_field_name}=" + CGI::escape(post_params(array_of_hashes, *args).gsub("\\", "").gsub(/\s?([\{\}])\s?/, '\1').gsub(/"\{/, '{').gsub(/\}"/, '}'))
       modify(endpoint, 'add', desired_class, array_of_hashes, array_field_name, delete_first_result, *args)
     end
+
+    def edit(endpoint, desired_class, array_of_hashes, array_field_name, delete_first_result, *args)
+      modify(endpoint, 'edit', desired_class, array_of_hashes, array_field_name, delete_first_result, *args)
+      #url = "http://api.toodledo.com/2/#{endpoint}/edit.php?key=#{self.key};#{array_field_name}=" + CGI::escape(post_params(array_of_hashes, *args).gsub("\\", "").gsub(/\s?([\{\}])\s?/, '\1').gsub(/"\{/, '{').gsub(/\}"/, '}'))
+    end
+
+    def delete(endpoint, desired_class, array_of_hashes, array_field_name, delete_first_result, *args)
+      modify(endpoint, 'delete', desired_class, array_of_hashes, array_field_name, delete_first_result, *args)
+      #url = "http://api.toodledo.com/2/#{endpoint}/delete.php?key=#{self.key};" + post_params(list_of_hash_endpoint_objs, *args) 
+      Typhoeus::Request.post(url)
+    end
+    
+    def get_tasks(param_map, *args)
+      #Strip off the first element, since it is not a Task
+      array_of_json_tasks = get("tasks", param_map, *args)
+      array_of_json_tasks[1..-1].collect{|json_task| Babar::Task.new(self, json_task)}
+    end
+
 
     def add_task(list_of_hash_tasks, *args)
       #Can accept either a single task (as a Hash) or an Array of tasks (each represented as a Hash)
@@ -149,28 +157,32 @@ module Babar
       #url = "http://api.toodledo.com/2/tasks/add.php?key=#{self.key};tasks=" + CGI::escape(post_params(list_of_hash_tasks, *args).gsub("\\", "").gsub(/\s?([\{\}])\s?/, '\1').gsub(/"\{/, '{').gsub(/\}"/, '}'))
       add('tasks', Babar::Task, list_of_hash_tasks, 'tasks', false)
     end
-
-    def edit(endpoint, desired_class, array_of_hashes, array_field_name, delete_first_result, *args)
-      modify(endpoint, 'edit', desired_class, array_of_hashes, array_field_name, delete_first_result, *args)
-      #url = "http://api.toodledo.com/2/#{endpoint}/edit.php?key=#{self.key};#{array_field_name}=" + CGI::escape(post_params(array_of_hashes, *args).gsub("\\", "").gsub(/\s?([\{\}])\s?/, '\1').gsub(/"\{/, '{').gsub(/\}"/, '}'))
-    end
-
     def edit_tasks(list_of_hash_tasks, *args)
       #TODO implement error checking on :id and object type
       list_of_hash_tasks = [list_of_hash_tasks,] if list_of_hash_tasks.is_a Hash
       edit("tasks", Babar::Task, list_of_hash_tasks, 'tasks', false, *args)
     end
 
-    def delete(endpoint, desired_class, array_of_hashes, array_field_name, delete_first_result, *args)
-      modify(endpoint, 'delete', desired_class, array_of_hashes, array_field_name, delete_first_result, *args)
-      #url = "http://api.toodledo.com/2/#{endpoint}/delete.php?key=#{self.key};" + post_params(list_of_hash_endpoint_objs, *args) 
-      Typhoeus::Request.post(url)
-    end
-    
+       
     def delete_tasks(list_of_task_ids, *args)
       #TODO check that this will work because array is of IDs, not hashes
       modify(endpoint, 'delete', desired_class, array_of_hashes, array_field_name, delete_first_result, *args*)
     end
+
+
+    def get_locations(param_map={}, *args)
+      array_of_json_locations = get('locations', {}, *args)
+      array_of_json_locations.collect{|json_location| Babar::Location.new(self, json_location)}
+    end
+
+    def add_locations(single_location_hash, *args)
+      #TODO enforce :name presence
+      #Currently, it appears that the API only supports adding one location at a time
+      #However, this will be treated as a list, for the sake of modularity (and potential future compatibility)
+      list_of_hash_locations = [single_location_hash,] if list_of_hash_locations.is_a Hash
+      add('locations', Babar::Location, list_of_hash_locations, 'locations', false)
+    end
+
 
   end
 
