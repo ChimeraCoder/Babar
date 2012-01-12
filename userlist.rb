f3988a1c136f54819e01f46bac4b6adc4f5e0698
aditya <dev@chimeracoder.net>
2012-01-12 14:57:05 -0500

    UserList now implements the retrieve method

--- userlist.rb
+++ userlist.rb
@@ -2,17 +2,38 @@ module Babar
 
   class UserList
     
+    #TODO enforce IMMEDIATE save when ID is defined/not known
+    #Otherwise, retrieve will not work
+    #Also, it is a bad idea not to have an ID defined
+    #
+    #
+    #This constructor should NEVER be called to initialize a new object by the user
+    #The user should create all objects through the User object
+    #For example, Babar::User.new_context()
+    #The .new_#{foo} method will call the appropriate constructor internally
+    #The .new_#{foo} method will also hit the /add endpoint, so that this constructor can be used internally without infinite recursion
     def initialize(authenticator, json_parsed)
       @authenticator, @json_parsed = authenticator, json_parsed
     end
 
-    def synchronize
-      #TODO fill this in
-    end
-
     def retrieve(field)
       #TODO fill this in
-      return :retrieve
+      class_name = self.class
+      ary = @authenticator.send("get_#{class_name}".downcase)
+      ary.each do |obj|
+        if obj.id == self.id
+          result = obj
+          break
+        end
+      end
+      @json_parsed.merge!(obj.json_parsed)
+      #Use json_parsed here, or else you may end up in an infinite loop of calls to retrieve 
+      obj.json_parsed.fetch(field, nil)
     end
+
+    def delete
+      #TODO implement this
+    end 
+
   end
 end
