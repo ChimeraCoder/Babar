dbfa6de0d77ef801521500be5b7d5fb3a82d3106
aditya <dev@chimeracoder.net>
2012-01-12 14:57:40 -0500

    Context/Folder/Goal/Location now inherit from UserList

--- context.rb
+++ context.rb
@@ -1,27 +1,26 @@
 module Babar
 
-  class Context 
+  class Context < UserList
 
     #TODO issue a warning when ID is changed
     attr_accessor :id
+    attr_accessor :json_parsed
 
-    def initialize(toodledo, json_parsed)
-      @toodledo, @json_parsed = toodledo, json_parsed
+    def initialize(authenticator, json_parsed)
+      super(authenticator, json_parsed)
       @id = json_parsed["id"].to_i
     end
 
     %w(name).each do |field|
-      define_method(field) { retrieve field unless @json_parsed.has_key?(field) }
+      define_method(field) do
+        @json_parsed.fetch(field, retrieve(field))
+      end
       #The following syntax only works in Ruby 1.9 and above
-      define_method "#{field}=" do |sync_now = false|
-        @json_parsed.has_key?(field)
+      define_method "#{field}=" do |value, sync_now = false|
+        @json_parsed[field] = value
         synchronize if sync_now
       end
     end
-
-
-    def synchronize
-      #TODO fill this in
     end
 
-end
+ end
--- location.rb
+++ location.rb
@@ -1,24 +1,25 @@
 module Babar
 
-  class Location
+  class Location < UserList
 
-    def initialize(toodledo, json_parsed)
-      @toodledo, @json_parsed = toodledo, json_parsed
+    attr_accessor :id
+    attr_accessor :json_parsed
+
+    def initialize(authenticator, json_parsed)
+      super(authenticator, json_parsed)
       @id = json_parsed["id"].to_i
     end
 
-    %w(name description lat long).each do |field|
-      define_method(field) { retrieve field unless @json_parsed.has_key?(field) }
+    %w(name description lat lon).each do |field|
+      define_method(field) do
+        @json_parsed.fetch(field, retrieve(field))
+      end
       #The following syntax only works in Ruby 1.9 and above
-      define_method "#{field}=" do |sync_now = false|
-        @json_parsed.has_key?(field)
+      define_method "#{field}=" do |value, sync_now = false|
+        @json_parsed[field] = value
         synchronize if sync_now
       end
     end
-
-
-    def synchronize
-      #TODO fill this in
     end
 
-end
+ end
