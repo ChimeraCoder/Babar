7587b4f6ae7db3225712ae7aa2edf1c3bfe010d6
aditya <dev@chimeracoder.net>
2012-01-18 03:25:04 -0500

    User object now has [add|edit]_[task|#{list}] methods

--- user.rb
+++ user.rb
@@ -11,12 +11,43 @@ module Babar
        @authenticator = Babar::Base.new(toodle_uid, toodle_password) 
       end
 
+      @tasks = {}
       @contexts = {}
       @goals = {}
       @folders = {}
       @locations {}
     end
 
+    def new_task(params = {})
+        #TODO figure out what to do about adding single/many tasks
+        raise ArgumentError if not params[:title] or params["title"]
+
+        #Query the API to add the task
+        task = @authenticator.add_task(params)
+        @tasks[task.id] = task
+    end
+
+    def edit_task(params = {})
+        #TODO figure out what to do about editing single/many tasks
+        raise ArgumentError if not params[:id] or params["id"]
+        #Query the API to edit the task
+        task = @authenticator.edit_task(params)
+        @tasks[task.id] = task
+    end
+
+    %w(context folder goal location).each do |list|
+       define_method("new_#{list}") do |params|  
+         #TODO figure out how to access instance variable by name properly
+         result = @authenticator.send("add_#{list}", params)
+         self.send("#{list}s").store(result.id, result)
+       end
+       define_method("edit_#{list}") do |params|
+         raise ArgumentError if not params[:id] or params["id"]
+         result = @authenticator.send("edit_#{list}", params)
+         self.send("#{list}s").store(result.id, result)
+       end
+
+        
 
   end
 end
